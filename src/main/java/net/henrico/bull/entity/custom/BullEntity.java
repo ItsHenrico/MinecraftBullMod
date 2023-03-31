9d5185292aec902592af0113b5257712f6ed4258
ItsHenrico <186ocirneh@gmail.com>
2023-03-31 15:00:59 +0200

    a

--- src/main/java/net/henrico/bull/entity/custom/BullEntity.java
+++ src/main/java/net/henrico/bull/entity/custom/BullEntity.java
@@ -1,5 +1,6 @@
 package net.henrico.bull.entity.custom;
 
+import net.henrico.bull.entity.ModEntityTypes;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
@@ -17,20 +18,19 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 import org.w3c.dom.Attr;
-import software.bernie.geckolib3.core.IAnimatable;
-import software.bernie.geckolib3.core.PlayState;
-import software.bernie.geckolib3.core.builder.AnimationBuilder;
-import software.bernie.geckolib3.core.builder.ILoopType;
-import software.bernie.geckolib3.core.controller.AnimationController;
-import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
-import software.bernie.geckolib3.core.manager.AnimationData;
-import software.bernie.geckolib3.core.manager.AnimationFactory;
-import software.bernie.geckolib3.util.GeckoLibUtil;
+import software.bernie.geckolib.animatable.GeoEntity;
+import software.bernie.geckolib.core.animatable.GeoAnimatable;
+import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
+import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
+import software.bernie.geckolib.core.animation.AnimatableManager;
+import software.bernie.geckolib.core.animation.Animation;
+import software.bernie.geckolib.core.animation.AnimationController;
+import software.bernie.geckolib.core.object.PlayState;
 
 import javax.management.Attribute;
 
-public class BullEntity extends Animal implements IAnimatable {
-    private AnimationFactory factory = GeckoLibUtil.createFactory(this);
+public class BullEntity extends Animal implements GeoEntity {
+    private AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
     public BullEntity(EntityType<? extends Animal> pEntityType, Level plevel) {
         super(pEntityType, plevel);
     }
@@ -46,53 +46,35 @@ public class BullEntity extends Animal implements IAnimatable {
 
     protected void registerGoals(){
         this.goalSelector.addGoal(1, new FloatGoal(this));
-        this.goalSelector.addGoal(2, new PanicGoal(this, 1.250));
-        this.goalSelector.addGoal(3, new LookAtPlayerGoal(this, Player.class, 8.0f));
-        this.goalSelector.addGoal(4, new WaterAvoidingRandomStrollGoal(this, 1.0D));
-        this.goalSelector.addGoal(5 ,new RandomLookAroundGoal(this));
-        this.goalSelector.addGoal(6, (new HurtByTargetGoal(this)).setAlertOthers());
-    }
-
-    private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event){
-        if(event.isMoving()){
-            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.bull.walk", ILoopType.EDefaultLoopTypes.LOOP));
-            return PlayState.CONTINUE;
-        }
-
-        event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.bull.idle", ILoopType.EDefaultLoopTypes.LOOP));
-        return PlayState.CONTINUE;
-    }
-
-    @Override
-    public void registerControllers(AnimationData data) {
-        data.addAnimationController(new AnimationController<BullEntity>(this, "Controller", 0, this::predicate));
-    }
-
-    @Override
-    public AnimationFactory getFactory() {
-        return this.factory;
-    }
-
-    protected void playStepsound(BlockPos pos, BlockState BlockIn) {
-        this.playSound(SoundEvents.COW_STEP, 0.15f, 0.1f);
-    }
-
-    protected SoundEvent getAmbientSound() {
-        return SoundEvents.COW_AMBIENT;
-    }
-    protected SoundEvent getHurtSound(DamageSource damageSourceIn) {
-        return SoundEvents.COW_HURT;
-    }
-    protected SoundEvent getDeathSound() {
-        return SoundEvents.COW_DEATH;
-    }
-    protected float getSoundVolume() {
-        return 0.2f;
+        this.goalSelector.addGoal(3, new PanicGoal(this, 1.250));
+        this.goalSelector.addGoal(4, new LookAtPlayerGoal(this, Player.class, 8.0f));
+        this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 1.0D));
+        this.goalSelector.addGoal(6,new RandomLookAroundGoal(this));
+        this.goalSelector.addGoal(7, (new HurtByTargetGoal(this)).setAlertOthers());
     }
 
     @Nullable
     @Override
     public AgeableMob getBreedOffspring(ServerLevel serverLevel, AgeableMob ageableMob) {
-        return null;
+        return ModEntityTypes.BULL.get().create(level);
+    }
+
+    @Override
+    public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar) {
+        controllerRegistrar.add(new AnimationController<>(this, "controller", 0, this::predicate));
+    }
+
+    private <T extends GeoAnimatable> PlayState predicate(AnimationState<T> tAnimationState) {
+        if(animationState.isMoving()) {
+            animationState.getController().setAnimation(RanAnimation.begin().then("animation.bull.walk", Animation.LoopType.LOOP));
+            return PlayState.CONTINUE;
+        }
+
+        animationState.getController().setAnimation(RanAnimation.begin()).then("animation.bull.idle", Animation.LoopType.LOOP);
+        return PlayState.CONTINUE;
+    }
+    @Override
+    public AnimatableInstanceCache getAnimatableInstanceCache() {
+        return cache;
     }
 }
--- src/main/java/net/henrico/bull/event/ModEventBusEvents.java
+++ src/main/java/net/henrico/bull/event/ModEventBusEvents.java
@@ -1,12 +1,14 @@
 package net.henrico.bull.event;
 
+import net.henrico.bull.Bull;
 import net.henrico.bull.entity.ModEntityTypes;
 import net.henrico.bull.entity.custom.BullEntity;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod.EventBusSubscriber(modid = Bull.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModEventBusEvents {
-
     @SubscribeEvent
     public static void entityAttributeEvent(EntityAttributeCreationEvent event){
         event.put(ModEntityTypes.BULL.get(), BullEntity.setAttributes());
